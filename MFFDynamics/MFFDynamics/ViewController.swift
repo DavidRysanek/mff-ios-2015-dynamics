d81b77b8bad02e5a3637c9da721faba6544f1947
David Ryšánek <rysanek@avast.com>
2016-01-14 21:11:12 +0100

    + Displaying basic shapes (views)

--- MFFDynamics/MFFDynamics/ViewController.swift
+++ MFFDynamics/MFFDynamics/ViewController.swift
@@ -11,7 +11,19 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let square = UIView(frame: CGRect(x: 150, y: 70, width: 100, height: 100))
+        square.backgroundColor = UIColor.greenColor()
+        view.addSubview(square)
+        
+        let barrier = UIView(frame: CGRect(x: 0, y: 400, width: 130, height: 20))
+        barrier.backgroundColor = UIColor.grayColor()
+        view.addSubview(barrier)
+        
+        let circle = UIView(frame: CGRect(x: 200, y: 200, width: 80, height: 80))
+        circle.backgroundColor = UIColor.grayColor()
+        circle.layer.cornerRadius = circle.frame.size.width / 2
+        view.addSubview(circle)
     }
     
 }
